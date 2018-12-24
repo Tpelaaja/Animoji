01bf5d71e2570b9920fb83eaf0265ac6d70e4950
Sank6 <25014241+Sank6@users.noreply.github.com>
2018-12-24 07:37:31 +0000

    Add support for BFD Api

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const { Client } = require('klasa');
 const DBL = require("dblapi.js");
 const dbl = process.env.dbl;
 
+const BFD = require("bfd-api");
+const bfd = new BFD(process.env.bfd);
+
 class client extends Client {
   setDBL(dbl) {this.dbl = dbl;}
 };
@@ -15,10 +18,24 @@ const bot = new client({
 bot.on('ready', () => {
   bot.user.setActivity("for -help", { type: "WATCHING" });
   bot.setDBL(new DBL(dbl, bot));
+
+  let guilds = await client.shard.broadcastEval('this.guilds.size');
+  bfd.postCount(guilds.reduce((a, b) => a + b, 0), "448527818855284756");
 });
+
+bot.login();
 
 bot.on('commandRun', (message, command, args) =>
   console.log(`[COMMAND] ${command.name} in ${message.guild.name}`)
 );
 
-bot.login();
+// BFD post count
+bot.on('guildCreate', () => {
+  let guilds = await client.shard.broadcastEval('this.guilds.size');
+  bfd.postCount(guilds.reduce((a, b) => a + b, 0), "448527818855284756");
+})
+
+bot.on('guildDelete', () => {
+  let guilds = await client.shard.broadcastEval('this.guilds.size');
+  bfd.postCount(guilds.reduce((a, b) => a + b, 0), "448527818855284756");
+})
