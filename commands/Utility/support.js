04d656b081c190193fbac09585164e3456a11956
Sank6 <25014241+Sank6@users.noreply.github.com>
2018-12-26 05:40:02 +0000

    remove extra line

--- commands/Utility/support.js
+++ commands/Utility/support.js
@@ -11,7 +11,7 @@ module.exports = class extends Command {
     async run(message) {
       let e = new MessageEmbed()
       .setColor(process.env.theme)
-      .setDescription(`<:dblSmile:376811626197811200> Upvote me on [Discord Bot List](https://discordbots.org/bot/448527818855284756/vote).\n
+      .setDescription(`<:dblSmile:376811626197811200> Upvote me on [Discord Bot List](https://discordbots.org/bot/448527818855284756/vote).
 <:botsfordiscord2:381674698993303553> Upvote me on [Bots for Discord](https://botsfordiscord.com/bots/448527818855284756/vote)
 <:patreon:452871851409014824> Support me on [patreon](https://www.patreon.com/emojify)`)
       message.channel.send(e)
