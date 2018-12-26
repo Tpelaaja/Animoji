6c38888c2498feb3f146f4545e98ee84aa850cda
Sank6 <25014241+Sank6@users.noreply.github.com>
2018-12-26 05:39:01 +0000

    Add BFD Vote option in support command

--- commands/Utility/support.js
+++ commands/Utility/support.js
@@ -11,8 +11,9 @@ module.exports = class extends Command {
     async run(message) {
       let e = new MessageEmbed()
       .setColor(process.env.theme)
-      .setDescription(`<:dblSmile:376811626197811200> Upvote the bot on [Discord Bot List](https://discordbots.org/bot/448527818855284756/vote).\n
-<:patreon:452871851409014824> Support the bot on [patreon](https://www.patreon.com/emojify)`)
+      .setDescription(`<:dblSmile:376811626197811200> Upvote me on [Discord Bot List](https://discordbots.org/bot/448527818855284756/vote).\n
+<:botsfordiscord2:381674698993303553> Upvote me on [Bots for Discord](https://botsfordiscord.com/bots/448527818855284756/vote)
+<:patreon:452871851409014824> Support me on [patreon](https://www.patreon.com/emojify)`)
       message.channel.send(e)
     }
 };
