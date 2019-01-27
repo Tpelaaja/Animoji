eceb5f2dfe47aecab79a29971ec9acc8ebd79536
Sank6 <25014241+Sank6@users.noreply.github.com>
2019-01-27 15:39:19 +0000

    fix

--- commands/Fun/remove.js
+++ commands/Fun/remove.js
@@ -12,7 +12,7 @@ module.exports = class extends Command {
         });
     }
     async run(message, [ Emoji ]) {
-      Emoji = Emoji.split(':').join('')
+      if (Emoji.includes(':')) Emoji = Emoji.split(':')[1]
       if (!message.member.hasPermission('MANAGE_EMOJIS'))
         return message.channel.send(`<a:crossanimated:441425622187769877> You don't have enough permissions to remove emojis. You need \`Manage Emojis\`.`);
       if (!Emoji)
